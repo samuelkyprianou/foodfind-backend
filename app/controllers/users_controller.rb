954839fb5945d25700e0db531f65f98ffa3a2071
samuelkyprianou <samuelkyprianou@gmail.com>
2020-03-25 15:09:51 +0000

    fix favourite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     def sign_in
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
-          render json: { username: user.username, token: generate_token(id: user.id) }
+          render json: { username: user.username, token: generate_token(id: user.id), favourites: user.restaurants }
         else
           render json: { error: "Username or Password is invalid "}
         end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
       def validate
         if get_user
-          render json: { username: get_user.username, token: generate_token(id: get_user.id)}
+          render json: { username: get_user.username, token: generate_token(id: get_user.id), favourites: get_user.restaurants}
         else
           render json: { error: "You are not authorized" }
         end
